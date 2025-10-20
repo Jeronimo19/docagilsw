20a750ea51a165977c153196be2268682602bdb4
Abram2173 <127432561+Abram2173@users.noreply.github.com>
2025-10-20 17:05:59 -0600

    Redirección a dashboard con HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // Para las rutas
+import { HashRouter } from 'react-router-dom';  // Para las rutas
 import App from './App';  // Importa tu App.tsx
 import './index.css';  // Si tenés CSS global
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>  {/* Envuelve para que las rutas funcionen */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
