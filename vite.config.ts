82c6c1432155b316dfe93a120b8e350ec63b8b89
Jerónimo <127432939+Jeronimo19@users.noreply.github.com>
2025-11-27 10:54:43 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';  // Para resolver alias
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dart1/',  // ← ¡CLAVE! Apunta al subpath de tu repo en Pages
+  base: '/docagilsw/',  // ← ¡CLAVE! Apunta al subpath de tu repo en Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),  // ← Esto hace que @ funcione en runtime
